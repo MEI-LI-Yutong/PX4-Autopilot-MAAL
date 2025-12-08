cb73499de99ee775007ce5b09e332db04c0270dd
raiots <raiot.lee@hotmail.com>
2025-12-08 11:11:12 +0800

    feat: controller implement for tiltrotor in custom control

--- src/modules/mc_custom_control/mc_custom_control.cpp
+++ src/modules/mc_custom_control/mc_custom_control.cpp
@@ -38,10 +38,14 @@
 #include <cmath>
 #include <inttypes.h>
 
+using matrix::Vector3f;
+
 MulticopterCustomControl::MulticopterCustomControl() :
+	ModuleParams(nullptr),
 	px4::WorkItem(MODULE_NAME, px4::wq_configurations::nav_and_controllers),
 	_loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle"))
 {
+	_trajectory_setpoint = PositionControl::empty_trajectory_setpoint;
 }
 
 MulticopterCustomControl::~MulticopterCustomControl()
@@ -55,6 +59,9 @@ bool MulticopterCustomControl::init()
 		PX4_ERR("callback registration failed");
 		return false;
 	}
+
+	updateParams();
+	ParametersUpdated();
 
 	return true;
 }
@@ -80,49 +87,6 @@ int MulticopterCustomControl::task_spawn(int argc, char *argv[])
 	_task_id = -1;
 
 	return PX4_ERROR;
-}
-
-void MulticopterCustomControl::ComputeOutputs(float dt, float servo_out[3], float motor_out[3])
-{
-	(void)dt;
-
-	const bool armed = (_vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_ARMED);
-	const bool manual_valid = (_manual_control_setpoint.timestamp != 0)
-				  && (hrt_elapsed_time(&_manual_control_setpoint.timestamp) < 500_ms);
-
-	// 默认输出用于接口验证：servo0 = 0.5，motor0 = 20%
-	for (int i = 0; i < 3; i++) {
-		servo_out[i] = 0.0f;
-		motor_out[i] = 0.0f;
-	}
-
-	servo_out[0] = 0.5f;
-	motor_out[0] = 0.2f;
-
-	if (!armed || !manual_valid) {
-		return;
-	}
-
-	const float rate_damping = 0.1f;
-
-	float roll_sp = PX4_ISFINITE(_manual_control_setpoint.roll) ? _manual_control_setpoint.roll : 0.0f;
-	float pitch_sp = PX4_ISFINITE(_manual_control_setpoint.pitch) ? _manual_control_setpoint.pitch : 0.0f;
-	float yaw_sp = PX4_ISFINITE(_manual_control_setpoint.yaw) ? _manual_control_setpoint.yaw : 0.0f;
-	float throttle_sp = PX4_ISFINITE(_manual_control_setpoint.throttle) ? _manual_control_setpoint.throttle : -1.0f;
-
-	roll_sp = math::constrain(roll_sp - rate_damping * _angular_velocity.xyz[0], -1.0f, 1.0f);
-	pitch_sp = math::constrain(pitch_sp - rate_damping * _angular_velocity.xyz[1], -1.0f, 1.0f);
-	yaw_sp = math::constrain(yaw_sp - rate_damping * _angular_velocity.xyz[2], -1.0f, 1.0f);
-
-	servo_out[0] = roll_sp;
-	servo_out[1] = pitch_sp;
-	servo_out[2] = yaw_sp;
-
-	const float thrust = math::constrain((throttle_sp + 1.0f) * 0.5f, 0.0f, 1.0f);
-
-	for (int i = 0; i < 3; i++) {
-		motor_out[i] = thrust;
-	}
 }
 
 void MulticopterCustomControl::PublishServos(const float servo_out[3], hrt_abstime now)
@@ -172,6 +136,166 @@ void MulticopterCustomControl::PublishMotors(const float motor_out[3], hrt_absti
 	_actuator_motors_pub.publish(actuator_motors);
 }
 
+void MulticopterCustomControl::ParametersUpdated()
+{
+	UpdatePositionControlGains();
+	UpdateAttitudeControlGains();
+	UpdateRateControlGains();
+}
+
+void MulticopterCustomControl::UpdatePositionControlGains()
+{
+	_position_control.setPositionGains(Vector3f(_param_mpc_xy_p.get(), _param_mpc_xy_p.get(), _param_mpc_z_p.get()));
+
+	const Vector3f vel_p(_param_mpc_xy_vel_p_acc.get(), _param_mpc_xy_vel_p_acc.get(), _param_mpc_z_vel_p_acc.get());
+	const Vector3f vel_i(_param_mpc_xy_vel_i_acc.get(), _param_mpc_xy_vel_i_acc.get(), _param_mpc_z_vel_i_acc.get());
+	const Vector3f vel_d(_param_mpc_xy_vel_d_acc.get(), _param_mpc_xy_vel_d_acc.get(), _param_mpc_z_vel_d_acc.get());
+	_position_control.setVelocityGains(vel_p, vel_i, vel_d);
+
+	_position_control.setVelocityLimits(_param_mpc_xy_vel_max.get(),
+					    _param_mpc_z_vel_max_up.get(),
+					    _param_mpc_z_vel_max_dn.get());
+
+	_position_control.setThrustLimits(_param_mpc_thr_min.get(), _param_mpc_thr_max.get());
+	_position_control.setHorizontalThrustMargin(_param_mpc_thr_xy_marg.get());
+	_position_control.setTiltLimit(math::radians(_param_mpc_tiltmax_air.get()));
+
+	_takeoff.setSpoolupTime(_param_com_spoolup_time.get());
+	_takeoff.setTakeoffRampTime(_param_mpc_tko_ramp_t.get());
+}
+
+void MulticopterCustomControl::UpdateAttitudeControlGains()
+{
+	const Vector3f att_p(_param_mc_roll_p.get(), _param_mc_pitch_p.get(), _param_mc_yaw_p.get());
+	_attitude_control.setProportionalGain(att_p, _param_mc_yaw_weight.get());
+	_attitude_control.setRateLimit(Vector3f(
+					     math::radians(_param_mc_rollrate_max.get()),
+					     math::radians(_param_mc_pitchrate_max.get()),
+					     math::radians(_param_mc_yawrate_max.get())));
+}
+
+void MulticopterCustomControl::UpdateRateControlGains()
+{
+	const Vector3f rate_k(_param_mc_rollrate_k.get(), _param_mc_pitchrate_k.get(), _param_mc_yawrate_k.get());
+	_rate_control.setPidGains(
+		rate_k.emult(Vector3f(_param_mc_rollrate_p.get(), _param_mc_pitchrate_p.get(), _param_mc_yawrate_p.get())),
+		rate_k.emult(Vector3f(_param_mc_rollrate_i.get(), _param_mc_pitchrate_i.get(), _param_mc_yawrate_i.get())),
+		rate_k.emult(Vector3f(_param_mc_rollrate_d.get(), _param_mc_pitchrate_d.get(), _param_mc_yawrate_d.get())));
+
+	_rate_control.setIntegratorLimit(Vector3f(_param_mc_rr_int_lim.get(), _param_mc_pr_int_lim.get(),
+						_param_mc_yr_int_lim.get()));
+
+	_rate_control.setFeedForwardGain(Vector3f(_param_mc_rollrate_ff.get(), _param_mc_pitchrate_ff.get(),
+					_param_mc_yawrate_ff.get()));
+}
+
+bool MulticopterCustomControl::RunPositionControl(float dt, vehicle_attitude_setpoint_s &att_sp,
+		matrix::Vector3f &thrust_sp)
+{
+	if (!_vehicle_control_mode.flag_control_position_enabled || !_vehicle_control_mode.flag_control_velocity_enabled) {
+		_position_control.resetIntegral();
+		return false;
+	}
+
+	PositionControlStates states{};
+	states.position = Vector3f(_position.x, _position.y, _position.z);
+	states.velocity = Vector3f(_position.vx, _position.vy, _position.vz);
+	states.acceleration = Vector3f(_position.ax, _position.ay, _position.az);
+	states.yaw = matrix::Eulerf(matrix::Quatf(_attitude.q)).psi();
+
+	_position_control.setState(states);
+
+	trajectory_setpoint_s current_setpoint = PositionControl::empty_trajectory_setpoint;
+
+	if (_trajectory_setpoint_sub.updated()) {
+		_trajectory_setpoint_sub.copy(&_trajectory_setpoint);
+	}
+
+	current_setpoint = _trajectory_setpoint;
+	_position_control.setInputSetpoint(current_setpoint);
+
+	const bool hover_valid = (_hover_thrust_estimate.timestamp != 0)
+				 && (hrt_elapsed_time(&_hover_thrust_estimate.timestamp) < 1_s)
+				 && PX4_ISFINITE(_hover_thrust_estimate.hover_thrust);
+	const float hover_thrust = hover_valid ? _hover_thrust_estimate.hover_thrust : _param_mpc_thr_hover.get();
+	_position_control.updateHoverThrust(hover_thrust);
+	_position_control.setHoverThrust(hover_thrust);
+
+	// update constraints & takeoff handling
+	_vehicle_constraints_sub.update(&_vehicle_constraints);
+
+	if (!PX4_ISFINITE(_vehicle_constraints.speed_up) || (_vehicle_constraints.speed_up > _param_mpc_z_vel_max_up.get())) {
+		_vehicle_constraints.speed_up = _param_mpc_z_vel_max_up.get();
+	}
+
+	// 起飞判定：armed 且 setpoint/约束要求上升
+	const bool want_takeoff = _vehicle_control_mode.flag_armed &&
+				  (_vehicle_constraints.want_takeoff
+				   || (PX4_ISFINITE(current_setpoint.position[2]) && (current_setpoint.position[2] < states.position(2)))
+				   || (PX4_ISFINITE(current_setpoint.velocity[2]) && (current_setpoint.velocity[2] < 0.f))
+				   || (PX4_ISFINITE(current_setpoint.acceleration[2]) && (current_setpoint.acceleration[2] < 0.f)));
+
+	_takeoff.updateTakeoffState(_vehicle_control_mode.flag_armed, _land_detected.landed, want_takeoff,
+				    _vehicle_constraints.speed_up, false, hrt_absolute_time());
+
+	const bool not_taken_off = (_takeoff.getTakeoffState() < TakeoffState::rampup);
+
+	// during takeoff ramp, freeze accel feed-forward on vertical axis
+	if (_takeoff.getTakeoffState() == TakeoffState::rampup) {
+		current_setpoint.acceleration[2] = NAN;
+	}
+
+	if (not_taken_off) {
+		current_setpoint = PositionControl::empty_trajectory_setpoint;
+		current_setpoint.timestamp = hrt_absolute_time();
+		Vector3f(0.f, 0.f, 100.f).copyTo(current_setpoint.acceleration);
+		_position_control.resetIntegral();
+	}
+
+	const float tilt_limit_deg = (_takeoff.getTakeoffState() < TakeoffState::flight)
+				     ? _param_mpc_tiltmax_lnd.get() : _param_mpc_tiltmax_air.get();
+	_position_control.setTiltLimit(math::radians(tilt_limit_deg));
+
+	const float speed_up = _takeoff.updateRamp(dt, PX4_ISFINITE(_vehicle_constraints.speed_up)
+				  ? _vehicle_constraints.speed_up : _param_mpc_z_vel_max_up.get());
+	const float speed_down = PX4_ISFINITE(_vehicle_constraints.speed_down) ? _vehicle_constraints.speed_down :
+				 _param_mpc_z_vel_max_dn.get();
+
+	const float minimum_thrust = not_taken_off ? 0.f : _param_mpc_thr_min.get();
+	_position_control.setThrustLimits(minimum_thrust, _param_mpc_thr_max.get());
+
+	float max_speed_xy = _param_mpc_xy_vel_max.get();
+
+	if (PX4_ISFINITE(_position.vxy_max)) {
+		max_speed_xy = math::min(max_speed_xy, _position.vxy_max);
+	}
+
+	_position_control.setVelocityLimits(
+		max_speed_xy,
+		math::min(speed_up, _param_mpc_z_vel_max_up.get()),
+		math::max(speed_down, 0.f));
+
+	_position_control.setInputSetpoint(current_setpoint);
+
+	const bool pos_ok = _position_control.update(dt);
+
+	if (!pos_ok) {
+		return false;
+	}
+
+	vehicle_local_position_setpoint_s local_sp{};
+	_position_control.getLocalPositionSetpoint(local_sp);
+	local_sp.timestamp = hrt_absolute_time();
+	_local_pos_sp_pub.publish(local_sp);
+
+	_position_control.getAttitudeSetpoint(att_sp);
+	att_sp.timestamp = hrt_absolute_time();
+	_att_sp_pub.publish(att_sp);
+
+	thrust_sp = Vector3f(att_sp.thrust_body[0], att_sp.thrust_body[1], att_sp.thrust_body[2]);
+	return true;
+}
+
 void MulticopterCustomControl::Run()
 {
 	if (should_exit()) {
@@ -187,23 +311,18 @@ void MulticopterCustomControl::Run()
 	if (_parameter_update_sub.updated()) {
 		parameter_update_s param_update;
 		_parameter_update_sub.copy(&param_update);
+		updateParams();
+		ParametersUpdated();
 	}
 
-	if (_attitude_sub.updated()) {
-		_attitude_sub.copy(&_attitude);
-	}
-
-	if (_position_sub.updated()) {
-		_position_sub.copy(&_position);
-	}
-
-	if (_manual_control_setpoint_sub.updated()) {
-		_manual_control_setpoint_sub.copy(&_manual_control_setpoint);
-	}
-
-	if (_vehicle_status_sub.updated()) {
-		_vehicle_status_sub.copy(&_vehicle_status);
-	}
+	_vehicle_status_sub.update(&_vehicle_status);
+	_attitude_sub.update(&_attitude);
+	_position_sub.update(&_position);
+	_manual_control_setpoint_sub.update(&_manual_control_setpoint);
+	_vehicle_land_detected_sub.update(&_land_detected);
+	_vehicle_control_mode_sub.update(&_vehicle_control_mode);
+	_hover_thrust_estimate_sub.update(&_hover_thrust_estimate);
+	_vehicle_constraints_sub.update(&_vehicle_constraints);
 
 	perf_begin(_loop_perf);
 
@@ -216,14 +335,45 @@ void MulticopterCustomControl::Run()
 	const float dt = math::constrain(((_angular_velocity.timestamp_sample - _last_run) * 1e-6f), 0.0002f, 0.02f);
 	_last_run = _angular_velocity.timestamp_sample;
 
-	float servo_out[3] {};
-	float motor_out[3] {};
+	vehicle_attitude_setpoint_s att_sp{};
 
-	ComputeOutputs(dt, servo_out, motor_out);
+	if (RunPositionControl(dt, att_sp, _thrust_sp)) {
+		matrix::Quatf qd(att_sp.q_d);
+		_attitude_control.setAttitudeSetpoint(qd, att_sp.yaw_sp_move_rate);
+		_rates_sp = _attitude_control.update(matrix::Quatf(_attitude.q));
+	} else {
+		_rates_sp.zero();
+		_thrust_sp.zero();
+	}
 
-	const hrt_abstime now = hrt_absolute_time();
-	PublishServos(servo_out, now);
-	PublishMotors(motor_out, now);
+	matrix::Vector3f angular_accel; // not available, leave zero
+	const bool landed = _land_detected.landed;
+	matrix::Vector3f torque_sp = _rate_control.update(Vector3f(_angular_velocity.xyz), _rates_sp, angular_accel, dt,
+					  landed);
+
+	vehicle_rates_setpoint_s rates_setpoint{};
+	rates_setpoint.timestamp = hrt_absolute_time();
+	rates_setpoint.roll = _rates_sp(0);
+	rates_setpoint.pitch = _rates_sp(1);
+	rates_setpoint.yaw = _rates_sp(2);
+	rates_setpoint.thrust_body[0] = _thrust_sp(0);
+	rates_setpoint.thrust_body[1] = _thrust_sp(1);
+	rates_setpoint.thrust_body[2] = _thrust_sp(2);
+	_vehicle_rates_setpoint_pub.publish(rates_setpoint);
+
+	vehicle_thrust_setpoint_s vehicle_thrust_setpoint{};
+	vehicle_thrust_setpoint.timestamp_sample = _angular_velocity.timestamp_sample;
+	vehicle_thrust_setpoint.timestamp = hrt_absolute_time();
+	_thrust_sp.copyTo(vehicle_thrust_setpoint.xyz);
+	_vehicle_thrust_setpoint_pub.publish(vehicle_thrust_setpoint);
+
+	vehicle_torque_setpoint_s vehicle_torque_setpoint{};
+	vehicle_torque_setpoint.timestamp_sample = _angular_velocity.timestamp_sample;
+	vehicle_torque_setpoint.timestamp = hrt_absolute_time();
+	vehicle_torque_setpoint.xyz[0] = torque_sp(0);
+	vehicle_torque_setpoint.xyz[1] = torque_sp(1);
+	vehicle_torque_setpoint.xyz[2] = torque_sp(2);
+	_vehicle_torque_setpoint_pub.publish(vehicle_torque_setpoint);
 
 	perf_end(_loop_perf);
 }
--- src/modules/mc_custom_control/mc_custom_control.hpp
+++ src/modules/mc_custom_control/mc_custom_control.hpp
@@ -43,26 +43,44 @@
 #include <px4_platform_common/defines.h>
 #include <px4_platform_common/log.h>
 #include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
 #include <px4_platform_common/px4_work_queue/WorkItem.hpp>
 
 #include <drivers/drv_hrt.h>
 #include <mathlib/mathlib.h>
+#include <matrix/matrix/math.hpp>
+
+#include <lib/rate_control/rate_control.hpp>
+#include <modules/mc_att_control/AttitudeControl/AttitudeControl.hpp>
+#include <modules/mc_pos_control/PositionControl/PositionControl.hpp>
+#include <modules/mc_pos_control/Takeoff/Takeoff.hpp>
 
 #include <uORB/Publication.hpp>
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionCallback.hpp>
 #include <uORB/topics/actuator_motors.h>
 #include <uORB/topics/actuator_servos.h>
+#include <uORB/topics/hover_thrust_estimate.h>
 #include <uORB/topics/manual_control_setpoint.h>
 #include <uORB/topics/parameter_update.h>
+#include <uORB/topics/vehicle_attitude_setpoint.h>
 #include <uORB/topics/vehicle_angular_velocity.h>
 #include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_constraints.h>
 #include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/vehicle_local_position_setpoint.h>
 #include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/vehicle_thrust_setpoint.h>
+#include <uORB/topics/vehicle_torque_setpoint.h>
+#include <uORB/topics/trajectory_setpoint.h>
+#include <uORB/topics/vehicle_land_detected.h>
+#include <uORB/topics/vehicle_control_mode.h>
+#include <uORB/topics/vehicle_rates_setpoint.h>
 
 using namespace time_literals;
 
-class MulticopterCustomControl : public ModuleBase<MulticopterCustomControl>, public px4::WorkItem
+class MulticopterCustomControl : public ModuleBase<MulticopterCustomControl>, public ModuleParams,
+	public px4::WorkItem
 {
 public:
 	MulticopterCustomControl();
@@ -88,6 +106,11 @@ private:
 	void ComputeOutputs(float dt, float servo_out[3], float motor_out[3]);
 	void PublishServos(const float servo_out[3], hrt_abstime now);
 	void PublishMotors(const float motor_out[3], hrt_abstime now);
+	void ParametersUpdated();
+	void UpdatePositionControlGains();
+	void UpdateAttitudeControlGains();
+	void UpdateRateControlGains();
+	bool RunPositionControl(float dt, vehicle_attitude_setpoint_s &att_sp, matrix::Vector3f &thrust_sp);
 
 	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
 	uORB::SubscriptionCallbackWorkItem _angular_velocity_sub{this, ORB_ID(vehicle_angular_velocity)};
@@ -95,9 +118,19 @@ private:
 	uORB::Subscription _position_sub{ORB_ID(vehicle_local_position)};
 	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+	uORB::Subscription _trajectory_setpoint_sub{ORB_ID(trajectory_setpoint)};
+	uORB::Subscription _vehicle_land_detected_sub{ORB_ID(vehicle_land_detected)};
+	uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
+	uORB::Subscription _hover_thrust_estimate_sub{ORB_ID(hover_thrust_estimate)};
+	uORB::Subscription _vehicle_constraints_sub{ORB_ID(vehicle_constraints)};
 
 	uORB::Publication<actuator_motors_s> _actuator_motors_pub{ORB_ID(actuator_motors)};
 	uORB::Publication<actuator_servos_s> _actuator_servos_pub{ORB_ID(actuator_servos)};
+	uORB::Publication<vehicle_rates_setpoint_s> _vehicle_rates_setpoint_pub{ORB_ID(vehicle_rates_setpoint)};
+	uORB::Publication<vehicle_torque_setpoint_s> _vehicle_torque_setpoint_pub{ORB_ID(vehicle_torque_setpoint)};
+	uORB::Publication<vehicle_thrust_setpoint_s> _vehicle_thrust_setpoint_pub{ORB_ID(vehicle_thrust_setpoint)};
+	uORB::Publication<vehicle_local_position_setpoint_s> _local_pos_sp_pub{ORB_ID(vehicle_local_position_setpoint)};
+	uORB::Publication<vehicle_attitude_setpoint_s> _att_sp_pub{ORB_ID(vehicle_attitude_setpoint)};
 
 	perf_counter_t _loop_perf{nullptr};
 	hrt_abstime _last_run{0};
@@ -107,6 +140,70 @@ private:
 	vehicle_angular_velocity_s _angular_velocity{};
 	vehicle_attitude_s _attitude{};
 	vehicle_local_position_s _position{};
+	trajectory_setpoint_s _trajectory_setpoint{};
 	manual_control_setpoint_s _manual_control_setpoint{};
 	vehicle_status_s _vehicle_status{};
+	vehicle_land_detected_s _land_detected{};
+	vehicle_control_mode_s _vehicle_control_mode{};
+	vehicle_constraints_s _vehicle_constraints{};
+	hover_thrust_estimate_s _hover_thrust_estimate{};
+
+	PositionControl _position_control;
+	TakeoffHandling _takeoff;
+	AttitudeControl _attitude_control;
+	RateControl _rate_control;
+
+	matrix::Vector3f _rates_sp{};
+	matrix::Vector3f _thrust_sp{};
+
+	DEFINE_PARAMETERS(
+		(ParamFloat<px4::params::MC_ROLL_P>) _param_mc_roll_p,
+		(ParamFloat<px4::params::MC_PITCH_P>) _param_mc_pitch_p,
+		(ParamFloat<px4::params::MC_YAW_P>) _param_mc_yaw_p,
+		(ParamFloat<px4::params::MC_YAW_WEIGHT>) _param_mc_yaw_weight,
+		(ParamFloat<px4::params::MC_ROLLRATE_MAX>) _param_mc_rollrate_max,
+		(ParamFloat<px4::params::MC_PITCHRATE_MAX>) _param_mc_pitchrate_max,
+		(ParamFloat<px4::params::MC_YAWRATE_MAX>) _param_mc_yawrate_max,
+
+		(ParamFloat<px4::params::MC_ROLLRATE_P>) _param_mc_rollrate_p,
+		(ParamFloat<px4::params::MC_PITCHRATE_P>) _param_mc_pitchrate_p,
+		(ParamFloat<px4::params::MC_YAWRATE_P>) _param_mc_yawrate_p,
+		(ParamFloat<px4::params::MC_ROLLRATE_I>) _param_mc_rollrate_i,
+		(ParamFloat<px4::params::MC_PITCHRATE_I>) _param_mc_pitchrate_i,
+		(ParamFloat<px4::params::MC_YAWRATE_I>) _param_mc_yawrate_i,
+		(ParamFloat<px4::params::MC_ROLLRATE_D>) _param_mc_rollrate_d,
+		(ParamFloat<px4::params::MC_PITCHRATE_D>) _param_mc_pitchrate_d,
+		(ParamFloat<px4::params::MC_YAWRATE_D>) _param_mc_yawrate_d,
+		(ParamFloat<px4::params::MC_ROLLRATE_K>) _param_mc_rollrate_k,
+		(ParamFloat<px4::params::MC_PITCHRATE_K>) _param_mc_pitchrate_k,
+		(ParamFloat<px4::params::MC_YAWRATE_K>) _param_mc_yawrate_k,
+		(ParamFloat<px4::params::MC_ROLLRATE_FF>) _param_mc_rollrate_ff,
+		(ParamFloat<px4::params::MC_PITCHRATE_FF>) _param_mc_pitchrate_ff,
+		(ParamFloat<px4::params::MC_YAWRATE_FF>) _param_mc_yawrate_ff,
+		(ParamFloat<px4::params::MC_RR_INT_LIM>) _param_mc_rr_int_lim,
+		(ParamFloat<px4::params::MC_PR_INT_LIM>) _param_mc_pr_int_lim,
+		(ParamFloat<px4::params::MC_YR_INT_LIM>) _param_mc_yr_int_lim,
+
+		(ParamFloat<px4::params::MPC_XY_P>) _param_mpc_xy_p,
+		(ParamFloat<px4::params::MPC_Z_P>) _param_mpc_z_p,
+		(ParamFloat<px4::params::MPC_XY_VEL_P_ACC>) _param_mpc_xy_vel_p_acc,
+		(ParamFloat<px4::params::MPC_XY_VEL_I_ACC>) _param_mpc_xy_vel_i_acc,
+		(ParamFloat<px4::params::MPC_XY_VEL_D_ACC>) _param_mpc_xy_vel_d_acc,
+		(ParamFloat<px4::params::MPC_Z_VEL_P_ACC>) _param_mpc_z_vel_p_acc,
+		(ParamFloat<px4::params::MPC_Z_VEL_I_ACC>) _param_mpc_z_vel_i_acc,
+		(ParamFloat<px4::params::MPC_Z_VEL_D_ACC>) _param_mpc_z_vel_d_acc,
+		(ParamFloat<px4::params::MPC_XY_VEL_MAX>) _param_mpc_xy_vel_max,
+		(ParamFloat<px4::params::MPC_Z_VEL_MAX_UP>) _param_mpc_z_vel_max_up,
+		(ParamFloat<px4::params::MPC_Z_VEL_MAX_DN>) _param_mpc_z_vel_max_dn,
+		(ParamFloat<px4::params::MPC_THR_MIN>) _param_mpc_thr_min,
+		(ParamFloat<px4::params::MPC_THR_MAX>) _param_mpc_thr_max,
+		(ParamFloat<px4::params::MPC_THR_HOVER>) _param_mpc_thr_hover,
+		(ParamFloat<px4::params::MPC_TILTMAX_AIR>) _param_mpc_tiltmax_air,
+		(ParamFloat<px4::params::MPC_TILTMAX_LND>) _param_mpc_tiltmax_lnd,
+		(ParamFloat<px4::params::MPC_THR_XY_MARG>) _param_mpc_thr_xy_marg,
+		(ParamFloat<px4::params::MPC_TKO_RAMP_T>) _param_mpc_tko_ramp_t,
+		(ParamFloat<px4::params::MPC_TKO_SPEED>) _param_mpc_tko_speed,
+		(ParamFloat<px4::params::MPC_LAND_SPEED>) _param_mpc_land_speed,
+		(ParamFloat<px4::params::COM_SPOOLUP_TIME>) _param_com_spoolup_time
+	)
 };
